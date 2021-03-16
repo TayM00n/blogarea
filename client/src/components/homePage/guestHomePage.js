5abced7b6d09e54175604a8caf43466821b76777
TayM00n <i.v.hroitskyi@gmail.com>
2021-03-16 17:22:35 +0200

    cosmetically change

--- client/src/components/homePage/guestHomePage.js
+++ client/src/components/homePage/guestHomePage.js
@@ -17,29 +17,34 @@ const GuestHomePage = () => {
   }
 
   const IconNext = ({width = 25, height = 25}) => {
-    return <svg width={width} height={height} viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"/>
+    return <svg width={width} height={height} viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z"
+        fill="currentColor" fillRule="evenodd" clipRule="evenodd"/>
     </svg>
   }
 
-  const images = ["https://placeimg.com/180/100/any","https://placeimg.com/180/100/people","https://placeimg.com/180/100/animal"]
+  const images = ["https://placeimg.com/180/100/any", "https://placeimg.com/180/100/people", "https://placeimg.com/180/100/animal"]
 
   return (
     <div className="hv-100 d-flex flex-column justify-content-center">
       <div className="customCarousel d-flex mx-auto">
         <div className='car-item'>
-          <img className='rounded-20' src={images[active-1]} alt={images[active-1].split("/")[images.length-1]}/>
+          <img className='rounded-20' src={images[active - 1]} alt={images[active - 1].split("/")[images.length - 1]}/>
         </div>
         <div className='car-manage d-flex flex-column justify-content-between ml-2'>
           {active - 1 !== 0 ?
-            <div className='back rounded-circle' onClick={() => handleSlideChange(active - 1 === 0 ? images.length : active - 1)}><IconBack/></div> :
+            <div className='back rounded-circle'
+                 onClick={() => handleSlideChange(active - 1 === 0 ? images.length : active - 1)}><IconBack/></div> :
             <div className='rounded-circle'><IconBack/></div>}
           <div className='car-pagination mx-auto d-flex flex-column justify-content-between'>
-            <div className={`${active === 1 ? "active" : ""}`} onClick={()=>handleSlideChange(1)}></div>
-            <div className={`${active === 2 ? "active" : ""}`} onClick={()=>handleSlideChange(2)}></div>
-            <div className={`${active === 3 ? "active" : ""}`} onClick={()=>handleSlideChange(3)}></div>
+            <div className={`${active === 1 ? "active" : ""}`} onClick={() => handleSlideChange(1)}></div>
+            <div className={`${active === 2 ? "active" : ""}`} onClick={() => handleSlideChange(2)}></div>
+            <div className={`${active === 3 ? "active" : ""}`} onClick={() => handleSlideChange(3)}></div>
           </div>
           {active + 1 <= images.length ?
-            <div className='next rounded-circle' onClick={() => handleSlideChange(active + 1 > images.length ? 1 : active + 1)}><IconNext/></div> :
+            <div className='next rounded-circle'
+                 onClick={() => handleSlideChange(active + 1 > images.length ? 1 : active + 1)}><IconNext/></div> :
             <div className='rounded-circle'><IconNext/></div>}
         </div>
       </div>
