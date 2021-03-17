34c50db8e41cd6351e1ad1fc8bbe90a5f357c6d8
TayM00n <i.v.hroitskyi@gmail.com>
2021-03-17 16:15:25 +0200

    edit authorPage.js

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ function App({global}) {
       "img": "https://placem.at/things?w=250&random=people",
       "stars": 3,
       "views": 20,
+      "date": "2021/01/21 8:42",
     }, {
       "id": 2,
       "author": "Vera Vorta",
@@ -31,6 +32,7 @@ function App({global}) {
       "img": "https://placem.at/things?w=250&random=2",
       "stars": 4,
       "views": 14,
+      "date": "2021/01/21 8:42",
     }, {
       "id": 3,
       "author": "Yort Mop",
@@ -39,6 +41,7 @@ function App({global}) {
       "img": "https://placem.at/things?w=250&random=sad",
       "stars": 2,
       "views": 44,
+      "date": "2021/01/21 8:42",
     }, {
       "id": 4,
       "author": "Boom Forta",
@@ -47,6 +50,7 @@ function App({global}) {
       "img": "https://placem.at/things?w=250&random=happy",
       "stars": 5,
       "views": 55,
+      "date": "2021/01/21 8:42",
     }, {
       "id": 5,
       "author": "Tetro Noper",
@@ -55,6 +59,7 @@ function App({global}) {
       "img": "https://placem.at/things?w=250&random=smille",
       "stars": 5,
       "views": 120,
+      "date": "2021/01/21 8:42",
     },
     {
       "id": 6,
@@ -64,6 +69,7 @@ function App({global}) {
       "img": "https://placem.at/things?w=250&random=apple",
       "stars": 3,
       "views": 20,
+      "date": "2021/01/21 8:42",
     }, {
       "id": 7,
       "author": "Vera Vorta",
@@ -72,6 +78,7 @@ function App({global}) {
       "img": "https://placem.at/things?w=250&random=bike",
       "stars": 4,
       "views": 14,
+      "date": "2021/01/21 8:42",
     }, {
       "id": 8,
       "author": "Yort Mop",
@@ -80,6 +87,7 @@ function App({global}) {
       "img": "https://placem.at/things?w=250&random=cat",
       "stars": 2,
       "views": 44,
+      "date": "2020/01/11 8:42",
     }, {
       "id": 9,
       "author": "Boom Forta",
@@ -88,6 +96,7 @@ function App({global}) {
       "img": "https://placem.at/things?w=250&random=dog",
       "stars": 5,
       "views": 55,
+      "date": "2021/02/4 18:40",
     }, {
       "id": 10,
       "author": "Tetro Noper",
@@ -96,7 +105,151 @@ function App({global}) {
       "img": "https://placem.at/things?w=250&random=girl",
       "stars": 5,
       "views": 120,
+      "date": "2021/01/11 8:42",
     }]
+
+  const users = [
+    {
+      "id": "GfOANm",
+      "firstName": "Alexie",
+      "lastName": "Hoeger",
+      "title": "Dynamic Integration Associate",
+      "jobTitle": "Senior Integration Planner",
+      "avatar": "https://placem.at/things?w=50&h=50&random=1",
+      "age": 36,
+      "country": "Djibouti",
+      "email": "alexie.hoeger@cfeo.io",
+      "phone": "634-774-7523",
+      "like": 52,
+      "dislike": 52
+    },
+    {
+      "id": "MYXpyK",
+      "firstName": "Rosario",
+      "lastName": "Tillman",
+      "title": "Internal Tactics Designer",
+      "jobTitle": "Chief Program Administrator",
+      "avatar": "https://placem.at/things?w=50&h=50&random=2",
+      "age": 65,
+      "country": "Maldives",
+      "email": "rosario.tillman@cfeo.io",
+      "phone": "990-509-6928",
+      "like": 52,
+      "dislike": 52
+    },
+    {
+      "id": "waQ3ID",
+      "firstName": "Vince",
+      "lastName": "Romaguera",
+      "title": "Dynamic Division Facilitator",
+      "jobTitle": "Dynamic Branding Agent",
+      "avatar": "https://placem.at/things?w=50&h=50&random=3",
+      "age": 21,
+      "country": "Myanmar",
+      "email": "vince.romaguera@cfeo.io",
+      "phone": "535-557-3554",
+      "like": 52,
+      "dislike": 52
+    },
+    {
+      "id": "aPzVVW",
+      "firstName": "Delphine",
+      "lastName": "McDermott",
+      "title": "Product Accountability Agent",
+      "jobTitle": "Global Identity Technician",
+      "avatar": "https://placem.at/things?w=50&h=50&random=4",
+      "age": 77,
+      "country": "Thailand",
+      "email": "delphine.mcdermott@cfeo.io",
+      "phone": "897-387-5681",
+      "like": 52,
+      "dislike": 52
+    },
+    {
+      "id": "nK8rsQ",
+      "firstName": "Christy",
+      "lastName": "Beahan",
+      "title": "Direct Paradigm Developer",
+      "jobTitle": "Forward Mobility Producer",
+      "avatar": "https://placem.at/things?w=50&h=50&random=5",
+      "age": 36,
+      "country": "Iraq",
+      "email": "christy.beahan@cfeo.io",
+      "phone": "300-793-3494",
+      "like": 52,
+      "dislike": 52
+    },
+    {
+      "id": "GIylBH",
+      "firstName": "Torrey",
+      "lastName": "Torphy",
+      "title": "Direct Functionality Facilitator",
+      "jobTitle": "Dynamic Identity Director",
+      "avatar": "https://placem.at/things?w=50&h=50&random=6",
+      "age": 73,
+      "country": "France",
+      "email": "torrey.torphy@cfeo.io",
+      "phone": "944-784-5805",
+      "like": 52,
+      "dislike": 52
+    },
+    {
+      "id": "iPg0nZ",
+      "firstName": "Austen",
+      "lastName": "Kovacek",
+      "title": "Central Solutions Analyst",
+      "jobTitle": "Investor Communications Liaison",
+      "avatar": "https://placem.at/things?w=50&h=50&random=7",
+      "age": 34,
+      "country": "Bosnia and Herzegovina",
+      "email": "austen.kovacek@cfeo.io",
+      "phone": "428-972-2615",
+      "like": 52,
+      "dislike": 52
+    },
+    {
+      "id": "NNvP08",
+      "firstName": "Magali",
+      "lastName": "Gibson",
+      "title": "Lead Identity Orchestrator",
+      "jobTitle": "Global Integration Producer",
+      "avatar": "https://placem.at/things?w=50&h=50&random=8",
+      "age": 23,
+      "country": "Jordan",
+      "email": "magali.gibson@cfeo.io",
+      "phone": "322-834-9909",
+      "like": 52,
+      "dislike": 52
+    },
+    {
+      "id": "XusUFV",
+      "firstName": "Ryley",
+      "lastName": "Flatley",
+      "title": "District Creative Consultant",
+      "jobTitle": "Dynamic Markets Associate",
+      "avatar": "https://placem.at/things?w=50&h=50&random=9",
+      "age": 52,
+      "country": "Albania",
+      "email": "ryley.flatley@cfeo.io",
+      "phone": "722-287-1248",
+      "like": 52,
+      "dislike": 52
+    },
+    {
+      "id": "vS9ltR",
+      "firstName": "Anais",
+      "lastName": "Oberbrunner",
+      "title": "Human Creative Developer",
+      "jobTitle": "Chief Implementation Administrator",
+      "avatar": "https://placem.at/things?w=50&h=50&random=10",
+      "age": 73,
+      "country": "American Samoa",
+      "email": "anais.oberbrunner@cfeo.io",
+      "phone": "570-740-9173",
+      "like": 52,
+      "dislike": 52
+    }
+  ]
 
   useEffect(() => {
     const rdItems = [
@@ -145,11 +298,11 @@ function App({global}) {
           <Route path="/settings" exec component={() => <h1>Settings</h1>}/>
           <Route path="/profile" exec component={() => <h1>Profile</h1>}/>
           <Route path="/createpost" exec component={() => <h1>Create post</h1>}/>
-          <Route path="/" exec component={() => <AuthorsPage isLogin={global.isLogin} posts={posts}/>}/>
+          <Route path="/" exec component={() => <AuthorsPage isLogin={global.isLogin} posts={posts} users={users}/>}/>
         </Switch>
       ) : (
         <Switch>
-          <Route path="/authors" exec component={() => <AuthorsPage isLogin={global.isLogin} posts={posts}/>}/>
+          <Route path="/authors" exec component={() => <AuthorsPage isLogin={global.isLogin} posts={posts} users={users}/>}/>
           <Route path="/login" exec component={() => <LogIn currentPage={global.currentPage}/>}/>
           <Route path="/signup" exec component={() => <SignUp currentPage={global.currentPage}/>}/>
           <Route path="/" exec component={GuestHomePage}/>
--- client/src/components/authors/authorsPage.js
+++ client/src/components/authors/authorsPage.js
@@ -2,11 +2,12 @@ import React, {useState} from "react";
 import {FormControl} from "react-bootstrap";
 import {Icons} from "../Icons";
 
-const AuthorsPage = ({isLogin, posts}) => {
+const AuthorsPage = ({isLogin, posts, users}) => {
 
   const [modePostsView, setModePostsView] = useState("All posts");
   const [numViews, setNumViews] = useState({posts:25, users:25})
   const [postsView, setPostsView] = useState([...posts])
+  const [usersView, setUsersView] = useState([...users])
 
   const sordPost = (arr, filter = "stars")=>{
     return arr.sort((a,b)=>{
@@ -31,28 +32,48 @@ const AuthorsPage = ({isLogin, posts}) => {
     return posts.map((post) => {
       return (
         <div className="post d-flex my-2" key={post.id}>
-          <div className='post-body row w-100'>
-            <div className='img mx-auto col-4'>
+          <div className='row post-body w-100 mx-auto'>
+            <div className='col-4 img mx-auto'>
               <img src={post.img} alt={post.title}/>
             </div>
-            <div className='col d-flex flex-column justify-content-between'>
-              <div className="post-title"><h1>post.title</h1></div>
+            <div className='col-md d-flex flex-column'>
+              <div className="post-title d-flex flex-row justify-content-between flex-wrap">
+                <h1>{post.title}</h1>
+                <pre className="text-muted">{post.author}</pre>
+              </div>
               <div className="post-description">
-                <p >{post.description}</p>
+                <p>{post.description}</p>
               </div>
-              <div className="post-footer d-flex justify-content-between">
-                <div className="author">
-                  {post.author}
+            </div>
+            <div className="row post-footer w-100 mx-auto">
+              <div className="author col-sm-7 d-flex justify-content-around">
+               <pre className='text-muted m-0 my-auto'> {post.date}</pre>
+              </div>
+              <div className='col-sm d-flex flex-row flex-wrap justify-content-around'>
+                <div className="stars mx-1">
+                  {[1, 2, 3, 4, 5].map(star => <Icons.IconEmptyStar key={star} isActive={post.stars >= star ? post.stars : false}/>)}
                 </div>
-                <div className='d-flex flex-row flex-wrap'>
-                  <div className="stars">
-                    {[1, 2, 3, 4, 5].map(star => <Icons.IconEmptyStar key={star} isActive={post.stars >= star ? post.stars : false}/>)}
-                  </div>
-                  <div className="views">
-                    <Icons.IconEya/>{" "+post.views}
-                  </div>
+                <div className="views mx-1">
+                  <Icons.IconEya/>{" "+post.views}
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
+  const Users = ({users}) => {
+    return users.map((user) => {
+      return (
+        <div className="user my-2 d-flex flex-row" key={user.id}>
+          <img src={user.avatar} alt="avatar" className="avatar-thumb my-auto"/>
+          <div className='user-body ml-1 w-100 my-auto text-center'>
+            <h6 className='lead'>{user.firstName + " " + user.lastName}</h6>
+            <div className='filling d-flex flex-row justify-content-around'>
+              <div className='reputation-like'><Icons.IconSmile/>{' ' + user.like}</div>
+             <div className='reputation-dislike'><Icons.IconSad/>{' ' + user.dislike}</div>
             </div>
           </div>
         </div>
@@ -80,7 +101,7 @@ const AuthorsPage = ({isLogin, posts}) => {
               <Posts posts={postsView}/>
             </div>
           </div>
-          <div className=" col users d-flex flex-column justify-content-center">
+          <div className="col users d-flex flex-column justify-content-center">
             <div className="page-view d-flex flex-row justify-content-around mx-auto">
               <p className={numViews.users === 25 ? "active": ""} onClick={()=>setNumViews({...numViews, users: 25})}>25</p>
               <p className={numViews.users === 50 ? "active" : ""} onClick={()=>setNumViews({...numViews, users: 50})}>50</p>
@@ -93,7 +114,7 @@ const AuthorsPage = ({isLogin, posts}) => {
               </FormControl>
             </div>
             <div className="container-users rounded-20">
-
+              <Users users={usersView}/>
             </div>
           </div>
         </div>
