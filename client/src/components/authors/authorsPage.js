f10477bf67ee3f4ca87228dc99eb5f8bfd4a2973
TayM00n <taymoon@ex.ua>
2021-03-18 11:21:49 +0200

    some improvements

--- client/src/components/authors/authorsPage.js
+++ client/src/components/authors/authorsPage.js
@@ -5,24 +5,24 @@ import {Icons} from "../Icons";
 const AuthorsPage = ({isLogin, posts, users}) => {
 
   const [modePostsView, setModePostsView] = useState("All posts");
-  const [numViews, setNumViews] = useState({posts:25, users:25})
+  const [numViews, setNumViews] = useState({posts: 25, users: 25})
   const [postsView, setPostsView] = useState([...posts])
   const [usersView, setUsersView] = useState([...users])
 
-  const sordPost = (arr, filter = "stars")=>{
-    return arr.sort((a,b)=>{
-      if(a[filter] > b[filter]) return -1
-      if(a[filter] < b[filter]) return 1
+  const sordPost = (arr, filter = "rating") => {
+    return arr.sort((a, b) => {
+      if (a[filter] > b[filter]) return -1
+      if (a[filter] < b[filter]) return 1
       return 0
     })
   }
 
-  const handleModePostsView = (e)=>{
+  const handleModePostsView = (e) => {
     setModePostsView(e.target.value)
 
-    if(e.target.value === "All posts"){
+    if (e.target.value === "All posts") {
       setPostsView([...posts])
-    }else{
+    } else {
       let tempAr = sordPost([...posts])
       setPostsView(tempAr)
     }
@@ -36,25 +36,34 @@ const AuthorsPage = ({isLogin, posts, users}) => {
             <div className='col-4 img mx-auto'>
               <img src={post.img} alt={post.title}/>
             </div>
-            <div className='col-md d-flex flex-column'>
-              <div className="post-title d-flex flex-row justify-content-between flex-wrap">
-                <h1>{post.title}</h1>
-                <pre className="text-muted">{post.author}</pre>
-              </div>
-              <div className="post-description">
-                <p>{post.description}</p>
-              </div>
-            </div>
-            <div className="row post-footer w-100 mx-auto">
-              <div className="author col-sm-7 d-flex justify-content-around">
-               <pre className='text-muted m-0 my-auto'> {post.date}</pre>
-              </div>
-              <div className='col-sm d-flex flex-row flex-wrap justify-content-around'>
-                <div className="stars mx-1">
-                  {[1, 2, 3, 4, 5].map(star => <Icons.IconEmptyStar key={star} isActive={post.stars >= star ? post.stars : false}/>)}
+            <div className='col-lg d-flex flex-column justify-content-between'>
+              <div>
+                <div className="post-title d-flex flex-row justify-content-between flex-wrap">
+                  <h1>{post.title}</h1>
                 </div>
-                <div className="views mx-1">
-                  <Icons.IconEya/>{" "+post.views}
+                <div className="post-description">
+                  <p>{post.description}</p>
+                </div>
+              </div>
+              <div className="row post-footer w-100 mx-auto">
+                <div className="date col-md p-0 my-auto mx-1 order-2 order-md-1">
+                  <pre className='text-muted m-0'> {post.date}</pre>
+                </div>
+                <div className='col-md p-0 my-auto mx-1 order-3 order-md-2'>
+                  <div className="d-flex flex-row justify-content-around">
+                    <div className="rating mx-1">
+                      <Icons.IconRating color={post.rating>0 ? "green" : "red"}/>{post.rating}
+                    </div>
+                    <div className="saved mx-1">
+                      <Icons.IconSaved/>{" " + post.saved}
+                    </div>
+                    <div className="views mx-1">
+                      <Icons.IconEya/>{" " + post.views}
+                    </div>
+                  </div>
+                </div>
+                <div className="more col-md my-auto p-0 mx-1 d-flex justify-content-end order-1 order-md-3">
+                  <a className="btn more my-auto">More<Icons.IconNextV2/></a>
                 </div>
               </div>
             </div>
@@ -73,7 +82,7 @@ const AuthorsPage = ({isLogin, posts, users}) => {
             <h6 className='lead'>{user.firstName + " " + user.lastName}</h6>
             <div className='filling d-flex flex-row justify-content-around'>
               <div className='reputation-like'><Icons.IconSmile/>{' ' + user.like}</div>
-             <div className='reputation-dislike'><Icons.IconSad/>{' ' + user.dislike}</div>
+              <div className='reputation-dislike'><Icons.IconSad/>{' ' + user.dislike}</div>
             </div>
           </div>
         </div>
@@ -87,9 +96,12 @@ const AuthorsPage = ({isLogin, posts, users}) => {
         <div className='row d-flex flex-row justify-content-between w-100'>
           <div className="col-lg-9 posts d-flex flex-column justify-content-center">
             <div className="page-view d-flex flex-row justify-content-around mx-auto">
-              <p className={numViews.posts === 25 ? "active":""} onClick={()=>setNumViews({...numViews, posts: 25})}>25</p>
-              <p className={numViews.posts === 50 ? "active":""} onClick={()=>setNumViews({...numViews, posts: 50})}>50</p>
-              <p className={numViews.posts === 100 ? "active":""} onClick={()=>setNumViews({...numViews, posts: 100})}>100</p>
+              <p className={numViews.posts === 25 ? "active" : ""}
+                 onClick={() => setNumViews({...numViews, posts: 25})}>25</p>
+              <p className={numViews.posts === 50 ? "active" : ""}
+                 onClick={() => setNumViews({...numViews, posts: 50})}>50</p>
+              <p className={numViews.posts === 100 ? "active" : ""}
+                 onClick={() => setNumViews({...numViews, posts: 100})}>100</p>
             </div>
             <div className="custom-select-authors mx-auto">
               <FormControl as="select" className="px-5" onChange={handleModePostsView}>
@@ -103,9 +115,12 @@ const AuthorsPage = ({isLogin, posts, users}) => {
           </div>
           <div className="col users d-flex flex-column justify-content-center">
             <div className="page-view d-flex flex-row justify-content-around mx-auto">
-              <p className={numViews.users === 25 ? "active": ""} onClick={()=>setNumViews({...numViews, users: 25})}>25</p>
-              <p className={numViews.users === 50 ? "active" : ""} onClick={()=>setNumViews({...numViews, users: 50})}>50</p>
-              <p className={numViews.users === 100 ? "active" : ""} onClick={()=>setNumViews({...numViews, users: 100})}>100</p>
+              <p className={numViews.users === 25 ? "active" : ""}
+                 onClick={() => setNumViews({...numViews, users: 25})}>25</p>
+              <p className={numViews.users === 50 ? "active" : ""}
+                 onClick={() => setNumViews({...numViews, users: 50})}>50</p>
+              <p className={numViews.users === 100 ? "active" : ""}
+                 onClick={() => setNumViews({...numViews, users: 100})}>100</p>
             </div>
             <div className="custom-select-users mx-auto">
               <FormControl as="select" className="px-5">
--- client/src/components/homePage/guestHomePage.js
+++ client/src/components/homePage/guestHomePage.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import {Icons} from "../Icons";
 
 const GuestHomePage = () => {
 
@@ -6,22 +7,6 @@ const GuestHomePage = () => {
 
   const handleSlideChange = (id) => {
     setActive(id)
-  }
-
-  const IconBack = ({width = 25, height = 25}) => {
-    return <svg width={width} height={height} viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M3.13523 8.84197C3.3241 9.04343 3.64052 9.05363 3.84197 8.86477L7.5 5.43536L11.158 8.86477C11.3595 9.05363 11.6759 9.04343 11.8648 8.84197C12.0536 8.64051 12.0434 8.32409 11.842 8.13523L7.84197 4.38523C7.64964 4.20492 7.35036 4.20492 7.15803 4.38523L3.15803 8.13523C2.95657 8.32409 2.94637 8.64051 3.13523 8.84197Z"
-        fill="currentColor" fillRule="evenodd" clipRule="evenodd"/>
-    </svg>
-  }
-
-  const IconNext = ({width = 25, height = 25}) => {
-    return <svg width={width} height={height} viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z"
-        fill="currentColor" fillRule="evenodd" clipRule="evenodd"/>
-    </svg>
   }
 
   const images = ["/slide1.png", "/slide2.png", "/slide3.png"]
@@ -35,8 +20,8 @@ const GuestHomePage = () => {
         <div className='car-manage d-flex flex-column justify-content-between ml-2'>
           {active - 1 !== 0 ?
             <div className='back rounded-circle'
-                 onClick={() => handleSlideChange(active - 1 === 0 ? images.length : active - 1)}><IconBack/></div> :
-            <div className='rounded-circle'><IconBack/></div>}
+                 onClick={() => handleSlideChange(active - 1 === 0 ? images.length : active - 1)}><Icons.IconBack/></div> :
+            <div className='rounded-circle'><Icons.IconBack/></div>}
           <div className='car-pagination mx-auto d-flex flex-column justify-content-between '>
             <div className={`${active === 1 ? "active" : ""}`} onClick={() => handleSlideChange(1)}></div>
             <div className={`${active === 2 ? "active" : ""}`} onClick={() => handleSlideChange(2)}></div>
@@ -44,8 +29,8 @@ const GuestHomePage = () => {
           </div>
           {active + 1 <= images.length ?
             <div className='next rounded-circle'
-                 onClick={() => handleSlideChange(active + 1 > images.length ? 1 : active + 1)}><IconNext/></div> :
-            <div className='rounded-circle'><IconNext/></div>}
+                 onClick={() => handleSlideChange(active + 1 > images.length ? 1 : active + 1)}><Icons.IconNext/></div> :
+            <div className='rounded-circle'><Icons.IconNext/></div>}
         </div>
       </div>
     </div>
