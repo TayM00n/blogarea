c417d398d713377b52ff1b2107b7f8abe7f2751e
TayM00n <taymoon@ex.ua>
2021-03-26 18:38:48 +0200

    Realization of article details

--- client/src/components/authors/detailesAboutPost/detailesAboutPost.js
+++ client/src/components/authors/detailesAboutPost/detailesAboutPost.js
@@ -1,24 +1,56 @@
 import React from "react"
 import {connect} from "react-redux";
-import {Post} from "../authorsPage";
+import {Link} from "react-router-dom";
+import {Icons} from "../../Icons";
 
-const TempDetailsAboutPost = ({post}) => {
+const TempDetailsAboutPost = ({post, isLogin}) => {
+
+  const Post = ({post}) => {
+    return (
+      <div className="">
+        <div className='text-center'>
+          <h1>{post.title}</h1>
+        </div>
+        <div className='row d-flex flex-row flex-wrap'>
+          <div className='col-md-3'>
+            <img src={"https://placem.at/things?w=250&h=250&random=" + post.img} alt="1" style={{width: "100%"}}/>
+          </div>
+          <div className='col-md'>
+            {/*<p>id -- {post.id}</p>*/}
+
+            <p>Description -- {post.description}</p>
+            <p>Author -- {post.author}</p>
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className="fullPage">
-      {/*<h1>Post Details</h1>
-      <img src={"https://placem.at/things?w=250&h=250&random="+post.img} alt="1"/>
-      <p>id -- {post.id}</p>
-      <p>Author -- {post.author}</p>
-      <p>Title -- {post.title}</p>
-      <p>Description -- {post.description}</p>
-      <p>Rating -- {post.rating}</p>
-      <p>Views -- {post.views}</p>
-      <p>Saved -- {post.saved}</p>
-      <p>Date -- {post.date}</p>*/}
+    <div className={`fullPage container d-flex flex-column justify-content-center hv-100`}>
+      <div className='d-flex flex-column bg-light rounded-20 p-5'>
+        <Post post={post}/>
+      </div>
+      <div className='d-flex flex-row justify-content-between px-3'>
+        <pre className="text-muted">{post.date}</pre>
+        <div className="d-flex flex-row justify-content-around">
+          <div className="rating mx-2">
+            <Icons.IconRating
+              color={post.rating > 0 ? "green" : post.rating === 0 ? "currentColor" : "red"}/>{post.rating}
+          </div>
+          <div className="saved mx-2">
+            <Icons.IconSaved/>{" " + post.saved}
+          </div>
+          <div className="views mx-2">
+            <Icons.IconEya/>{" " + post.views}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-const DetailsAboutPost = connect()(TempDetailsAboutPost)
+const DetailsAboutPost = connect((state)=>({isLogin: state.globalReducer.isLogin}))(TempDetailsAboutPost)
 
 export default DetailsAboutPost;
